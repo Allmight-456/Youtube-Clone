61b804b1868ec3f6327cc7fb7a414d4ecd4de45e
Ishan Kumar <ishankumar@Ishans-MacBook-Air.local>
2024-05-28 22:05:13 +0530

    added logo and button

--- week-8-tailwind_clas/src/components/AppBar.tsx
+++ week-8-tailwind_clas/src/components/AppBar.tsx
@@ -1,16 +1,17 @@
 import { SearchBar } from "../components/SearchBar";
+import { SignIn } from "./SignIn";
 
 export const AppBar = () => {
   return (
     <div className="flex justify-between items-center p-4">
       <div className="basis-1/4">
-        Youtube
+        <img src="logo1.png" className="w-20 h-15" />
       </div>
       <div className="basis-1/2 flex justify-center">
         <SearchBar />
       </div>
       <div className="basis-1/4 flex justify-end">
-        Sign In
+        <SignIn />
       </div>
     </div>
   );
---
+++ week-8-tailwind_clas/src/components/signIn.tsx
@@ -0,0 +1,10 @@
+export const SignIn = () => {
+    return (
+      <div className="flex items-center">
+        <button className="bg-slate-950 text-gray-400 font-bold py-2 px-4 rounded-3xl inline-flex items-center border border-gray-500">
+          Sign In
+        </button>
+      </div>
+    );
+  };
+  
